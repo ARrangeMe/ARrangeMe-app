5956b07984cf5905f782b23c60b9d80d66685d02
Anson Tang <32215233+ansontang216@users.noreply.github.com>
2021-11-09 02:44:14 -0500

    created unit test module

---
+++ unit-tests/src/main/java/com/example/unit_tests/UnitTests.java
@@ -0,0 +1,13 @@
+package com.example.unit_tests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnitTests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_tests);
+    }
+}
