112469c346350e8932324039344b7539e368fa6a
Anson Tang <32215233+ansontang216@users.noreply.github.com>
2021-11-10 01:53:27 -0500

    added unit test folder/dependencies. can't build

---
+++ app/src/test/java/com/google/ar/sceneform/samples/src/model/ItemTest.java
@@ -0,0 +1,32 @@
+package com.google.ar.sceneform.samples.src.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ItemTest {
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void setHeight() {
+    }
+
+    @Test
+    void setWidth() {
+    }
+
+    @Test
+    void setLength() {
+    }
+}
