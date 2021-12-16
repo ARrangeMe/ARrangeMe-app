f82b907d1bae5960417724a8fa0586552ddd8b49
roberttoyonaga <robert.toyonaga@gmail.com>
2021-12-16 18:47:58 -0500

    add constants

---
+++ app/src/main/java/com/google/ar/sceneform/samples/src/services/Constants.java
@@ -0,0 +1,6 @@
+package com.google.ar.sceneform.samples.src.services;
+
+public class Constants {
+    public static String jobsEndpoint =
+            "https://y4ff702tki.execute-api.us-east-2.amazonaws.com/test/jobs";
+}
--- app/src/main/java/com/google/ar/sceneform/samples/src/ui/login/LoginPresenterImpl.java
+++ app/src/main/java/com/google/ar/sceneform/samples/src/ui/login/LoginPresenterImpl.java
@@ -1,7 +1,7 @@
 package com.google.ar.sceneform.samples.src.ui.login;
 
 import com.google.ar.sceneform.samples.src.model.JobsList;
-import com.google.ar.sceneform.samples.src.model.PackingStrategy;
+import com.google.ar.sceneform.samples.src.services.Constants;
 import com.google.ar.sceneform.samples.src.services.SortingServerService;
 import com.google.gson.Gson;
 
@@ -16,7 +16,7 @@ public class LoginPresenterImpl implements LoginPresenter {
 
         //use service to make get request
         try {
-            String response = service.get("https://y4ff702tki.execute-api.us-east-2.amazonaws.com/test/jobs", "{request body}");
+            String response = service.get(Constants.jobsEndpoint, "{request body}");
             JobsList jobs = gson.fromJson(response, JobsList.class);
             return jobs;
         } catch (Exception e) {
